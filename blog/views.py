4bd7eb08d829bf6f57ccf07b3dbe1c51277b2f46
mgrami <m.grami.m@gmail.com>
2022-08-09 12:01:58 -0700

    search functionality

--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,14 @@ class PostListView(generic.ListView):
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
 
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            result = Post.objects.filter(title__contains=query)
+        else:
+            result = Post.objects.all()
+        return result
+
 
 class PostDetailView(generic.DetailView):
     model = Post
